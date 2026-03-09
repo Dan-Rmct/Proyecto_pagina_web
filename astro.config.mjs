db5014987c63b01721d8f7332a1d93fd16ef6953
Daniel Ramirez <daniel.rmct@gmail.com>
2026-03-08 23:31:14 -0600

    Fix del error de css

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://dan-rmct.github.io",
+  base: "/Proyecto_pagina_web/"
   vite: {
     plugins: [tailwindcss()]
   }
